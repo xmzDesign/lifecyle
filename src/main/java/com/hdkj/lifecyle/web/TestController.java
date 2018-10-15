476eb507a62cc1e5957aaa366a60cefe048f62ee
xuminzhe <864263105@qq.com>
2018-10-15 21:52:16 +0800

    新建TestController

---
+++ src/main/java/com/hdkj/lifecyle/web/TestController.java
@@ -0,0 +1,12 @@
+package com.hdkj.lifecyle.web;
+
+/**
+ * @author Xu.MinZhe
+ * @version V1.0
+ * @Project lifecyle
+ * @Package com.hdkj.lifecyle.web
+ * @Description
+ * @Date 2018/10/15 21:42
+ */
+public class TestController {
+}
