2596e3be1697b0e1a5915f0666a010923829be72
xuminzhe <864263105@qq.com>
2018-10-16 09:49:35 +0800

    add log

--- src/main/java/com/hdkj/lifecyle/web/IndexController.java
+++ src/main/java/com/hdkj/lifecyle/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.hdkj.lifecyle.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("index")
 public class IndexController {
+private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+    
 
     @GetMapping("IndexController")
     public String index() throws Exception {
         return "IndexController";
     }
+    
+    
 }
 
